6b4da14c32485c38ab110a3007c73995c26c4d7c
Awele <awele.osuka@gmail.com>
2020-07-14 00:23:54 +0100

    next article

---
+++ content/blog/api/index.md
@@ -0,0 +1,7 @@
+---
+title: What's an API?
+date: "2020-07-13T01:07:06.284Z"
+tags: ["API", "rest", "REST API", "rest api"]
+---
+
+An API is something that connects two systems together
--- src/html.js
+++ src/html.js
@@ -18,6 +18,7 @@ function HTML({
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         {headComponents}
+      
       </head>
       <body {...bodyAttributes} className="light">
         <script
@@ -30,6 +31,8 @@ function HTML({
         {preBodyComponents}
         <div key="body" id="___gatsby" dangerouslySetInnerHTML={{ __html: body }} />
         {postBodyComponents}
+      
+
       </body>
     </html>
   );
